cc381bd293104c52d0e80e90e4ac009191a9cdcc
Krish2Ski <seth9240@hallcrossacademy.co.uk>
2025-10-15 13:15:39 +0100

    Convert ListGroup and Message to JS and add comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import Message from "./Message";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
     <div>
+      <Message />
       <ListGroup
+        title="Cities"
         items={["London", "Doncaster", "Sheffield", "Liverpool", "Newcastle"]}
-        onSelectItem={(item) => console.log("You picked:", item)} 
+        onSelectItem={(item) => console.log("You picked:", item)}
       />
     </div>
   );
