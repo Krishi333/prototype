2a78114b609597b2ba03589628941669884da968
JahnaviC2021 <jchalla2021@gmail.com>
2025-10-25 21:29:11 +0100

    moved logo to assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import Message from "./Message";
 import ListGroup from "./components/ListGroup";
+// import LoginPage from "./components/LoginPages/LoginPage"
+// import RegistrationPage from "./components/LoginPages/RegistrationPage";
+import EntryPage from "./components/LoginPages/EntryPage";
+import { useState } from "react";
+
+
 // testing 
 function App() {
+  const [isEntry, setIsEntry] = useState(true);
   return (
     <div>
-      <Message />
+      {/* const [isEntry, setIsEntry] = useState(true); */}
+
+      {/* <Message />
       <ListGroup
         title="Cities"
-        items={["London", "Doncaster", "Sheffield", "Guildford", "Manchester", "ben"]}
+        items={["London", "Doncaster", "Sheffield", "Guildford", "Manchester"]}
         onSelectItem={(item) => console.log("You picked:", item)}
-      />
+      /> */}
+      <EntryPage />
+      <main />
+      
     </div>
   );
 }
