ba7154844a1eebce9bfd0a9ff75efae694cfa782
JahnaviC2021 <jchalla2021@gmail.com>
2025-11-02 23:08:48 +0000

    added jsx of managerStats

---
+++ src/components/Pie.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Pie} from "react-chartjs-2";
+import {Chart as ChartJS, 
+        Title,
+        Tooltip,
+        Legend,
+        ArcElement,
+
+        } from "chart.js";
+
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement
+);
+
+const PieChart = ({ data, options }) => {
+    return <Pie options={options} data={data} />
+};
+
+export default PieChart;
+
