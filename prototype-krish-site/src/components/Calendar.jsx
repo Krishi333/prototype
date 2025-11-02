4e60184c97d540e3632433609ff3d15c966b0132
Ben Miller <B.J.Miller1-24@student.lboro.ac.uk>
2025-11-02 23:09:05 +0000

    Removing calendar built-in sidebar

--- prototype-krish-site/src/components/Calendar.jsx
+++ prototype-krish-site/src/components/Calendar.jsx
@@ -16,9 +16,6 @@ function Calendar() {
 
   return (
     <div style={{ display: 'flex', height: '100vh' }}>
-      <div style={{ width: '200px' }}>
-        <CalSideBar />
-      </div>
       <div style={{ flex: 1, padding: '20px' }}>
         <Dates onClickTask={handleTaskClick} />
         {showTaskDetails && (
