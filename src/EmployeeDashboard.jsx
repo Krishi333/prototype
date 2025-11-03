5ad42ff7734590730a7936af016989935347a767
Ben Miller <B.J.Miller1-24@student.lboro.ac.uk>
2025-11-03 14:00:13 +0000

    Pushing Maya's fully onto the screen

--- src/EmployeeDashboard.jsx
+++ src/EmployeeDashboard.jsx
@@ -1,14 +1,15 @@
 import Header from "./EmployeeComponents/Header.jsx";
 import TabGroup from "./EmployeeComponents/Toggles.jsx";
 import Navbar from "./EmployeeComponents/Navbar.jsx";
+import "./EmployeeComponents/tabs.css";
 
 function EmployeeDashboard() {
   return (
-    <>
+    <div style={{marginLeft: "5%"}}>
       <Header />
       <TabGroup />
       <Navbar />
-    </>
+    </div>
   );
 }
 
